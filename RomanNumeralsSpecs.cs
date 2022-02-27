488905c9c1f5523e8c635b8a221d7aa8d76fc2b2
Felix Kröner <felix.kroener@gmx.de>
2022-02-27 21:37:08 +0100

    Implement conversion of 5

--- RomanNumeralsSpecs.cs
+++ RomanNumeralsSpecs.cs
@@ -10,12 +10,19 @@ public class RomanNumeralsSpecs
     {
         RomenNumeral.For(1).Should().Be("I");
     }
+    
+    [Fact]
+    public void Integer_5_maps_onto_V()
+    {
+        RomenNumeral.For(5).Should().Be("V");
+    }
 }
 
 public static class RomenNumeral
 {
     public static string For(int i)
     {
+        if (i == 5) return "V";
         return "I";
     }
 }
