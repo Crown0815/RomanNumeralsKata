ae5a1afb17b4232f9c1065d3041b092664a3c6bd
Felix Kröner <felix.kroener@gmx.de>
2022-02-27 21:41:48 +0100

    Implement missing roman character integers

--- RomanNumeralsSpecs.cs
+++ RomanNumeralsSpecs.cs
@@ -8,6 +8,11 @@ public class RomanNumeralsSpecs
     [Theory]
     [InlineData(1, 'I')]
     [InlineData(5, 'V')]
+    [InlineData(10, 'X')]
+    [InlineData(50, 'L')]
+    [InlineData(100, 'C')]
+    [InlineData(500, 'D')]
+    [InlineData(1000, 'M')]
     public void Integer_maps_onto_character(int integer, char roman)
     {
         RomenNumeral.For(integer).Should().Be(roman.ToString());
@@ -19,6 +24,11 @@ public static class RomenNumeral
     public static string For(int integer)
     {
         if (integer == 5) return "V";
+        if (integer == 10) return "X";
+        if (integer == 50) return "L";
+        if (integer == 100) return "C";
+        if (integer == 500) return "D";
+        if (integer == 1000) return "M";
         return "I";
     }
 }
