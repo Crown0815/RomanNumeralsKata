b3c78bc036b77daf404a4ab2515518a0af31554c
Felix Kröner <felix.kroener@gmx.de>
2022-02-27 21:36:13 +0100

    Implement conversion of 1

--- RomanNumeralsSpecs.cs
+++ RomanNumeralsSpecs.cs
@@ -1,7 +1,21 @@
+using FluentAssertions;
 using Xunit;
 
 namespace RomanNumerals;
 
 public class RomanNumeralsSpecs
 {
+    [Fact]
+    public void Integer_1_maps_onto_I()
+    {
+        RomenNumeral.For(1).Should().Be("I");
+    }
+}
+
+public static class RomenNumeral
+{
+    public static string For(int i)
+    {
+        return "I";
+    }
 }
