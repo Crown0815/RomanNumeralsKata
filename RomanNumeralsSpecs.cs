cd49b101008867de07d6a2fd274530ea9d96b83f
Felix Kröner <felix.kroener@gmx.de>
2022-02-27 21:39:20 +0100

    Refactor

--- RomanNumeralsSpecs.cs
+++ RomanNumeralsSpecs.cs
@@ -5,24 +5,20 @@ namespace RomanNumerals;
 
 public class RomanNumeralsSpecs
 {
-    [Fact]
-    public void Integer_1_maps_onto_I()
+    [Theory]
+    [InlineData(1, 'I')]
+    [InlineData(5, 'V')]
+    public void Integer_maps_onto_character(int integer, char roman)
     {
-        RomenNumeral.For(1).Should().Be("I");
-    }
-    
-    [Fact]
-    public void Integer_5_maps_onto_V()
-    {
-        RomenNumeral.For(5).Should().Be("V");
+        RomenNumeral.For(integer).Should().Be(roman.ToString());
     }
 }
 
 public static class RomenNumeral
 {
-    public static string For(int i)
+    public static string For(int integer)
     {
-        if (i == 5) return "V";
+        if (integer == 5) return "V";
         return "I";
     }
 }
